59c03224231e5e2d5e91b18fa35b51d23a8990e5
feixiao <feixiao2020@sina.com>
2021-06-01 15:18:25 +0800

    属性的使用

--- src/ch02/2.2.1_2_Properties1.kt
+++ src/ch02/2.2.1_2_Properties1.kt
@@ -1,12 +1,15 @@
 package ch02.person
 
+// 定义类和属性
 class Person(
     val name: String,           // val 常量
     var isMarried: Boolean      // var 变量
 )
 
 fun main(args: Array<String>) {
+    // 创建类
     val person = Person("Bob", true)
+    
     println(person.name)
     println(person.isMarried)
 }
--- src/ch02/2.2.2_CustomAccessors.kt
+++ src/ch02/2.2.2_CustomAccessors.kt
@@ -2,6 +2,7 @@ package ch02.ex2_2_CustomAccessors
 
 class Rectangle(val height: Int, val width: Int) {
     val isSquare: Boolean
+        // 自定义访问器
         get() {
             return height == width
         }
