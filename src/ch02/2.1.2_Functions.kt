4a3e6345392feae3bf7542cb5c8b235eb3eaa4fe
feixiao <feixiao2020@sina.com>
2021-06-01 15:15:02 +0800

    字符串模板使用

--- src/ch02/2.1.2_Functions.kt
+++ src/ch02/2.1.2_Functions.kt
@@ -1,9 +1,11 @@
 package ch02.ex1_2_Functions
 
+// 函数定义
 fun max(a: Int, b: Int): Int {
     return if (a > b) a else b
 }
 
 fun main(args: Array<String>) {
+    // 函数调用
     println(max(1, 2))
 }
--- src/ch02/2.1.4_3_StringTemplates2.kt
+++ src/ch02/2.1.4_3_StringTemplates2.kt
@@ -1,5 +1,7 @@
 package ch02.ex1_4_3_StringTemplates2
 
 fun main(args: Array<String>) {
+
+    // ${} 内部嵌入表达式
     println("Hello, ${if (args.size > 0) args[0] else "someone"}!")
 }
