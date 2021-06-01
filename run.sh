ffe13539d78a8b2d2a8ed97e86ea0ef35911bbf0
feixiao <feixiao2020@sina.com>
2021-06-01 15:41:12 +0800

    支持编译多文件

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 
-kotlinc $1 -include-runtime -d hello.jar
+echo $SHELL_FOLDER
+
+
+kt_file=$*
+
+kotlinc $kt_file -include-runtime -classpath ${SHELL_FOLDER}/src -d hello.jar 
 java -jar hello.jar
