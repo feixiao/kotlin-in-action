ad09b41bef3dd11fa53680180bc55c163d3a4d9b
feixiao <feixiao2020@sina.com>
2021-06-02 10:36:43 +0800

    data关键字

--- src/ch04/4.3.1.1_StringRepresentationToString.kt
+++ src/ch04/4.3.1.1_StringRepresentationToString.kt
@@ -1,5 +1,7 @@
 package ch04.ex3_1_1_StringRepresentationToString
 
+
+// data 关键字都解决了，所以这边的toString没有什么意义
 class Client(val name: String, val postalCode: Int) {
     override fun toString() = "Client(name=$name, postalCode=$postalCode)"
 }
--- src/ch04/4.3.1.2_2_ObjectEqualityEquals1.kt
+++ src/ch04/4.3.1.2_2_ObjectEqualityEquals1.kt
@@ -3,6 +3,7 @@ package ch04.ex3_1_2_2_ObjectEqualityEquals1
 class Client(val name: String, val postalCode: Int) {
     // 缺少hashCode方法,违反了通用hasCode契约, 如果两个对象相同必须含有相同的hash值
     // hashCode会自动生成
+    // data 关键字都解决了，所以这边的equals没有什么意义
     override fun equals(other: Any?): Boolean {
         if (other == null || other !is Client)
             return false
